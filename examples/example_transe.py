00702b550798fca7fd7d28787bcddd9bd7dbd0e0
Quester-one <2113809110@qq.com>
2021-10-23 14:52:08 +0800

    add example_transe

--- examples/example_transe.py
+++ examples/example_transe.py
@@ -1,2 +1,55 @@
-#transe
-print(123)
+#导入基本模块
+import torch
+import numpy as np
+import random
+import torch.utils.data as Data
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+#导入cogktr模块
+from cogktr import *
+
+#设置超参数
+random.seed(1)               #随机数种子
+np.random.seed(1)            #随机数种子
+DATASET_FILE_PATH="FB15k-237"#文件路径
+EMBEDDING_DIM=100            #形成的embedding维数
+MARGIN=1.0                   #margin大小
+L=2                          #范数类型
+LR=0.001                     #学习率
+EPOCH=50                     #训练的轮数
+BATCH_SIZE=2048              #批量大小
+WEIGHT_DECAY=0.0001          #正则化系数
+MODEL="transe"               #选择模型
+METRIC_SAMPLE_NUM=100        #评价采样个数
+METRIC_TEST_EPOCH=1000       #评价重复轮数
+
+#指定GPU
+device = torch.device('cuda:0')
+
+#加载原始数据集
+loader = FB15K237Loader("../dataset/Fb15k-237")
+train_data,valid_data,test_data=loader.load_all_data()
+entity2idx,relation2idx=loader.load_all_dict()
+
+#数据处理
+processor=FB15K237Processor("../dataset/Fb15k-237")
+train_datable=processor.process(train_data)
+valid_datable=processor.process(valid_data)
+test_datable=processor.process(test_data)
+
+#DataTableSet()暂时用如下替代
+train_dataset=Data.DataLoader(dataset=train_datable,batch_size=BATCH_SIZE,shuffle=True)
+valid_dataset=Data.DataLoader(dataset=valid_datable,batch_size=BATCH_SIZE,shuffle=True)
+test_dataset=Data.DataLoader(dataset=test_datable,batch_size=BATCH_SIZE,shuffle=True)
+
+#RandomSampler()还未写
+
+model=TransE(entity_dict_len=len(entity2idx),relation_dict_len=len(relation2idx),embedding_dim=EMBEDDING_DIM,margin=MARGIN,L=L)
+metric_1 =MeanRank(METRIC_SAMPLE_NUM,METRIC_TEST_EPOCH)
+metric_2 =HitTen(METRIC_SAMPLE_NUM,METRIC_TEST_EPOCH)
+loss =MarginLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=LR,weight_decay=WEIGHT_DECAY)
+# trainer = Trainer()
+# trainer.train()
