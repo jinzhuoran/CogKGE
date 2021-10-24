b55baa25b66c7bebb7d1e931070c0aa7ce692ad1
Quester-one <2113809110@qq.com>
2021-10-24 22:02:31 +0800

    update example_transe.py

--- examples/example_transe.py
+++ examples/example_transe.py
@@ -13,17 +13,15 @@ from cogktr import *
 #设置超参数
 random.seed(1)               #随机数种子
 np.random.seed(1)            #随机数种子
-DATASET_FILE_PATH="FB15k-237"#文件路径
 EMBEDDING_DIM=100            #形成的embedding维数
 MARGIN=1.0                   #margin大小
 L=2                          #范数类型
 LR=0.001                     #学习率
 EPOCH=10                     #训练的轮数
-BATCH_SIZE=2048              #批量大小
+BATCH_SIZE_TRAIN=2048        #训练批量大小
+BATCH_SIZE_TEST=100          #测试批量大小
 WEIGHT_DECAY=0.0001          #正则化系数
-MODEL="transe"               #选择模型
-METRIC_SAMPLE_NUM=100        #评价采样个数
-METRIC_TEST_EPOCH=1000       #评价重复轮数
+METRIC_TEST_EPOCH=200       #评价重复轮数
 
 #指定GPU
 
@@ -39,15 +37,14 @@ valid_datable=processor.process(valid_data)
 test_datable=processor.process(test_data)
 
 #DataTableSet()暂时用如下替代
-train_dataset=Data.DataLoader(dataset=train_datable,batch_size=BATCH_SIZE,shuffle=True)
-valid_dataset=Data.DataLoader(dataset=valid_datable,batch_size=BATCH_SIZE,shuffle=True)
-test_dataset=Data.DataLoader(dataset=test_datable,batch_size=BATCH_SIZE,shuffle=True)
+train_dataset=Data.DataLoader(dataset=train_datable,batch_size=BATCH_SIZE_TRAIN,shuffle=True)
+valid_dataset=Data.DataLoader(dataset=valid_datable,batch_size=BATCH_SIZE_TRAIN,shuffle=True)
+test_dataset=Data.DataLoader(dataset=test_datable,batch_size=BATCH_SIZE_TEST,shuffle=True)
 
 #RandomSampler()还未写
 
 model=TransE(entity_dict_len=len(entity2idx),relation_dict_len=len(relation2idx),embedding_dim=EMBEDDING_DIM,margin=MARGIN,L=L)
-metric_1 =MeanRank(sample_num=METRIC_SAMPLE_NUM,test_epoch=METRIC_TEST_EPOCH)
-metric_2 =HitTen(sample_num=METRIC_SAMPLE_NUM,test_epoch=METRIC_TEST_EPOCH)
+metric =MeanRank_HitAtTen(sample_num=BATCH_SIZE_TEST,test_epoch=METRIC_TEST_EPOCH,entity2idx_len=len(entity2idx))
 loss =MarginLoss(entity_dict_len=len(entity2idx))
 optimizer = torch.optim.Adam(model.parameters(), lr=LR,weight_decay=WEIGHT_DECAY)
 
@@ -56,12 +53,18 @@ trainer = Trainer(
     train_dataset=train_dataset,
     valid_dataset=valid_dataset,
     model=model,
-    metric=metric_1,
+    metric=metric,
     loss=loss,
     optimizer=optimizer,
     epoch=EPOCH
 )
 trainer.train()
 trainer.show()
-# evaluator=Evaluator()
-# evaluator.evaluate()
+
+#测试部分
+evaluator=Evaluator(
+    test_dataset=test_dataset,
+    model_path="TransE_Model_10epochs.pkl",
+    metric=metric
+)
+evaluator.evaluate()
