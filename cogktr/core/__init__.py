4495a951daa932ef16b2221dd38384c0f94dd770
Quester-one <2113809110@qq.com>
2021-10-26 16:41:03 +0800

    add base module

--- cogktr/core/__init__.py
+++ cogktr/core/__init__.py
@@ -1,10 +1,14 @@
 from .evaluator import *
+from .log import *
 from .loss import *
 from .metric import *
 from .trainer import *
 __all__=[
     #evaluate
     "Evaluator",
+
+    #log
+    "logger",
 
     #loss
     "MarginLoss",
