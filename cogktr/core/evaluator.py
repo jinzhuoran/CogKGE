00f6f8392697a055e6f81468ff3a41691d8c7355
Quester-one <2113809110@qq.com>
2021-10-26 21:09:15 +0800

    update logger

--- cogktr/core/evaluator.py
+++ cogktr/core/evaluator.py
@@ -19,7 +19,8 @@ class Evaluator:
         print("Model structure:\n", model)
         self.metric(self.test_dataset, model)
         print("result_rank_numpy:\n", self.metric.result_rank_numpy)
-        logger.info("mean_rank(total_sample_num_is_%d):%f%%" % (self.metric.sample_num, self.metric.mean_rank))
-        logger.info("hit_at_ten(total_epoch_num_is_%d):%f%%" % (self.metric.test_epoch, self.metric.hit_at_ten / self.metric.test_epoch * 100))
+        logger.info("mean_rank(total_sample_num_is_%d):%f" % (self.metric.sample_num, self.metric.mean_rank))
+        logger.info("hit_at_ten(total_sample_num_is_%d,total_epoch_num_is_%d):%f%%" % (self.metric.sample_num,self.metric.test_epoch,
+                                                                                       self.metric.hit_at_ten / self.metric.test_epoch * 100))
         print("The evaluating process is finished!")
         return 0
