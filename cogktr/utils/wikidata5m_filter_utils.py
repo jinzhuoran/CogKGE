2990dd12c63db6607a0454581c06b3e036ea0380
Quester-one <2113809110@qq.com>
2021-11-20 20:07:58 +0800

    update wikidata5m_filter_utils.py

--- cogktr/utils/wikidata5m_filter_utils.py
+++ cogktr/utils/wikidata5m_filter_utils.py
@@ -5,10 +5,11 @@ class WIKIDATA5M_Filter:
     def __init__(self,
                  path_wikidata5m,
                  path_mobilewikidata5m,
-                 threshold=1000):
+                 threshold=50000):
         self.path_wikidata5m = path_wikidata5m
         self.path_mobilewikidata5m = path_mobilewikidata5m
         self.threshold = threshold
+        self.entity_set=set()
 
     def filter_triplet(self, input_data_name, output_data_name, data_type):
         path_input = os.path.join(self.path_wikidata5m, input_data_name)
@@ -18,9 +19,14 @@ class WIKIDATA5M_Filter:
             for line in file_input:
                 if data_type == "train" or data_type == "valid":
                     h, r, t = line.strip().split("\t")
-                    if int(h[1:]) < 1000 and int(t[1:]) < 1000:
+                    if int(h[1:]) < self.threshold and int(t[1:]) < self.threshold:
+                        self.entity_set.add(h)
+                        self.entity_set.add(t)
                         file_output.write(line)
                 if data_type == "test":
+                    h, r, t = line.strip().split("\t")
+                    self.entity_set.add(h)
+                    self.entity_set.add(t)
                     file_output.write(line)
         file_output.close()
 
@@ -31,7 +37,9 @@ class WIKIDATA5M_Filter:
         with open(path_input) as file_input:
             for line in file_input:
                 text_list = line.strip().split("\t")
-                if int(text_list[0][1:]) < 1000:
+                if text_list[0] in self.entity_set:
+                # if int(text_list[0][1:]) < self.threshold and text_list[0] in self.entity_set:
+
                     file_output.write(line)
         file_output.close()
 
@@ -40,7 +48,7 @@ class WIKIDATA5M_Filter:
             raise ValueError("Path_wikidata5m is incorrect!Please download WIKIDATA5M!")
         if not os.path.exists(self.path_mobilewikidata5m):
             os.makedirs(self.path_mobilewikidata5m)
-            print(self.path_mobilewikidata5m + 'Create successfully!')
+            print(self.path_mobilewikidata5m +"\t"+'Create successfully!')
         else:
             print(self.path_mobilewikidata5m + 'The path has been existed!')
         self.filter_triplet(input_data_name="wikidata5m_transductive_train.txt",
@@ -59,5 +67,5 @@ class WIKIDATA5M_Filter:
 if __name__ == '__main__':
     filter = WIKIDATA5M_Filter(path_wikidata5m="../../dataset/kr/WIKIDATA5M/raw_data",
                                path_mobilewikidata5m="../../dataset/kr/MOBILEWIKIDATA5M/raw_data",
-                               threshold=1000)
+                               threshold=50000)
     filter.create_MOBILEWIKIDATA5M()
