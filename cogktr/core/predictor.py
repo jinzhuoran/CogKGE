52e05b044aaa393877e8700282a15ce0fb9065dc
Quester-one <2113809110@qq.com>
2022-01-15 15:17:52 +0800

    add more query functions

--- cogktr/core/predictor.py
+++ cogktr/core/predictor.py
@@ -64,6 +64,9 @@ class Kr_Predictior:
         self._create_name2id_relation_dict()
 
     def _create_name2id_node_dict(self):
+        """
+        建立节点名字到id的查找表
+        """
         if self.reprocess:
             for i in tqdm(range(self.node_len)):
                 item_df=self.node_lut[i]
@@ -81,6 +84,9 @@ class Kr_Predictior:
                 raise FileExistsError("{} does not exist!".format(self.name2id_node_dict_path))
 
     def _create_name2id_relation_dict(self):
+        """
+        建立关系名字到id的查找表
+        """
         if self.reprocess:
             for i in tqdm(range(self.relation_len)):
                 item_df = self.relation_lut[i]
@@ -96,6 +102,30 @@ class Kr_Predictior:
             else:
                 raise FileExistsError("{} does not exist!".format(self.name2id_relation_dict_path))
 
+    def _caculate_triplets_topk_node(self,triplet_id_matric):
+        """
+        计算三元组得分最高的k个头节点或者尾节点
+
+        :param triplet_id_matric: 更换头结点和尾节点的三元组
+        :return: 得分topk的三元组列表
+        """
+        distance = self.model(triplet_id_matric)
+        value, index = torch.topk(distance, self.top, largest=False)
+        node_list = []
+        for i in range(self.top):
+            id = int(index[i])
+            item = {}
+            item["name_id"] = self.node_lut[id]["name_id"]
+            item["name"] = self.node_lut[id]["name"]
+            item["name_rdf"] = self.node_lut[id]["name_rdf"]
+            item["type"] = self.node_lut[id]["type"]
+            item["type_rdf"] = self.node_lut[id]["type_rdf"]
+            item["node_type"] = self.node_lut[id]["node_type"]
+            item["description"] = self.node_lut[id]["description"]
+            item["confidence"] = value[i].item()
+            node_list.append(item)
+        return node_list
+
 
     def fuzzy_query_node_keyword(self,node_keyword=None):
         """
@@ -105,7 +135,6 @@ class Kr_Predictior:
         :return: 模糊节点列表
         """
         if node_keyword is None:
-            #做到这里了，每一种空值取10个
             return [self.name2id_node_dict["Copa Colombia"][0],
                     self.name2id_node_dict["Speed skating at the 2018 Winter Olympics – Women's 500 metres"][0],
                     self.name2id_node_dict["Swimming at the 1988 Summer Olympics – Women's 50 metre freestyle"][0],
@@ -169,66 +198,110 @@ class Kr_Predictior:
 
         return similar_node_list
 
-    def predcit_tail(self,head_id,relation_id):
+    def predcit_tail(self,head_id,relation_id=None):
         """
         根据头节点和关系预测尾节点
+        如果关系为空，则遍历所有关系，计算出每种关系得分最高的，选出前topk个节点
 
         :param head_id: 输入头节点id
-        :param relation_id: 输入关系
+        :param relation_id: 输入关系id
         :return: tail_list 尾实体列表
         """
+        if relation_id is None:
+            with torch.no_grad():
+                score_list=[] #每一个位置i对应关系i最好的得分
+                tail_index_list=[] #每一个位置i对应关系i最好的尾节点id
+                for relation_id in range(self.relation_len):
+                    head_id_vector = torch.tensor(head_id).expand(self.node_len, 1).to(self.device)
+                    relation_id_vector = torch.tensor(relation_id).expand(self.node_len, 1).to(self.device)
+                    tail_id_vectoe = self.all_node_index_column_matrix.clone()
+                    triplet_id_matric = torch.cat([head_id_vector, relation_id_vector, tail_id_vectoe], dim=1)
+                    distance = self.model(triplet_id_matric)
+                    tail_index = distance.argmin(dim=0)#关系i score最好的 尾节点id
+                    score = distance[tail_index]#关系i score最好的 得分数值
+                    score_list.append(score)
+                    tail_index_list.append(tail_index)
+                score = torch.tensor(score_list)
+                tail_index = torch.tensor(tail_index_list)
+                value, relation_index = torch.topk(score, self.top, largest=False)
+                tail_list = []
+                for i in range(self.top):
+                    tail_id = int(tail_index[i])
+                    relation_id=int(relation_index[i])
+                    item = {}
+                    item["name_id"] = self.node_lut[tail_id]["name_id"]
+                    item["name"] = self.node_lut[tail_id]["name"]
+                    item["name_rdf"] = self.node_lut[tail_id]["name_rdf"]
+                    item["type"] = self.node_lut[tail_id]["type"]
+                    item["type_rdf"] = self.node_lut[tail_id]["type_rdf"]
+                    item["node_type"] = self.node_lut[tail_id]["node_type"]
+                    item["description"] = self.node_lut[tail_id]["description"]
+                    item["confidence"] = value[i].item()
+                    item["triplet_id"]=(head_id,relation_id,tail_id)
+                    item["triplet_name"] = (self.node_lut[head_id]["name"],self.relation_lut[relation_id]["name"],self.node_lut[tail_id]["name"])
+                    tail_list.append(item)
+                return tail_list
+        else:
+            head_id_vector = torch.tensor(head_id).expand(self.node_len, 1).to(self.device)
+            relation_id_vector = torch.tensor(relation_id).expand(self.node_len, 1).to(self.device)
+            tail_id_vectoe = self.all_node_index_column_matrix.clone()
+            triplet_id_matric = torch.cat([head_id_vector, relation_id_vector, tail_id_vectoe], dim=1)
+            tail_list = self._caculate_triplets_topk_node(triplet_id_matric)
+            return tail_list
 
-        head_id_vector=torch.tensor(head_id).expand(self.node_len,1).to(self.device)
-        relation_id_vector=torch.tensor(relation_id).expand(self.node_len,1).to(self.device)
-        tail_id_vectoe=self.all_node_index_column_matrix.clone()
-        triplet_id_matric=torch.cat([head_id_vector,relation_id_vector,tail_id_vectoe],dim=1)
-        distance = self.model(triplet_id_matric)
-        value, index = torch.topk(distance, self.top, largest=False)
-        tail_list = []
-        for i in range(self.top):
-            id = int(index[i])
-            item = {}
-            item["name_id"] = self.node_lut[id]["name_id"]
-            item["name"] = self.node_lut[id]["name"]
-            item["name_rdf"] = self.node_lut[id]["name_rdf"]
-            item["type"] = self.node_lut[id]["type"]
-            item["type_rdf"] = self.node_lut[id]["type_rdf"]
-            item["node_type"] = self.node_lut[id]["node_type"]
-            item["description"] = self.node_lut[id]["description"]
-            item["confidence"] = value[i].item()
-            tail_list.append(item)
 
-        return tail_list
-
-    def predict_head(self,tail_id,relation_id):
+    def predict_head(self,tail_id,relation_id=None):
         """
         根据尾节点和关系预测头节点
+        如果关系为空，则遍历所有关系，计算出每种关系得分最高的，选出前topk个节点
 
         :param tail_id: 输入尾节点id
         :param relation_id: 输入关系id
         :return: head_list 头节点列表
         """
-        tail_id_vector = torch.tensor(tail_id).expand(self.node_len, 1).to(self.device)
-        relation_id_vector = torch.tensor(relation_id).expand(self.node_len, 1).to(self.device)
-        head_id_vector= self.all_node_index_column_matrix.clone()
-        triplet_id_matric = torch.cat([head_id_vector, relation_id_vector, tail_id_vector], dim=1)
-        distance = self.model(triplet_id_matric)
-        value, index = torch.topk(distance, self.top, largest=False)
-        head_list = []
-        for i in range(self.top):
-            id = int(index[i])
-            item = {}
-            item["name_id"] = self.node_lut[id]["name_id"]
-            item["name"] = self.node_lut[id]["name"]
-            item["name_rdf"] = self.node_lut[id]["name_rdf"]
-            item["type"] = self.node_lut[id]["type"]
-            item["type_rdf"] = self.node_lut[id]["type_rdf"]
-            item["node_type"] = self.node_lut[id]["node_type"]
-            item["description"] = self.node_lut[id]["description"]
-            item["confidence"] = value[i].item()
-            head_list.append(item)
+        if relation_id is None:
+            with torch.no_grad():
+                score_list = []  # 每一个位置i对应关系i最好的得分
+                head_index_list = []  # 每一个位置i对应关系i最好的头节点id
+                for relation_id in range(self.relation_len):
+                    tail_id_vector = torch.tensor(tail_id).expand(self.node_len, 1).to(self.device)
+                    relation_id_vector = torch.tensor(relation_id).expand(self.node_len, 1).to(self.device)
+                    head_id_vector = self.all_node_index_column_matrix.clone()
+                    triplet_id_matric = torch.cat([head_id_vector, relation_id_vector, tail_id_vector], dim=1)
+                    distance = self.model(triplet_id_matric)
+                    head_index = distance.argmin(dim=0)  # 关系i score最好的 头节点id
+                    score = distance[head_index]  # 关系i score最好的 得分数值
+                    score_list.append(score)
+                    head_index_list.append(head_index)
+                score = torch.tensor(score_list)
+                head_index = torch.tensor(head_index_list)
+                value, relation_index = torch.topk(score, self.top, largest=False)
+                head_list = []
+                for i in range(self.top):
+                    head_id = int(head_index[i])
+                    relation_id = int(relation_index[i])
+                    item = {}
+                    item["name_id"] = self.node_lut[head_id]["name_id"]
+                    item["name"] = self.node_lut[head_id]["name"]
+                    item["name_rdf"] = self.node_lut[head_id]["name_rdf"]
+                    item["type"] = self.node_lut[head_id]["type"]
+                    item["type_rdf"] = self.node_lut[head_id]["type_rdf"]
+                    item["node_type"] = self.node_lut[head_id]["node_type"]
+                    item["description"] = self.node_lut[head_id]["description"]
+                    item["confidence"] = value[i].item()
+                    item["triplet_id"] = (head_id, relation_id, tail_id)
+                    item["triplet_name"] = (self.node_lut[head_id]["name"], self.relation_lut[relation_id]["name"],
+                                            self.node_lut[tail_id]["name"])
+                    head_list.append(item)
+                return head_list
+        else:
+            tail_id_vector = torch.tensor(tail_id).expand(self.node_len, 1).to(self.device)
+            relation_id_vector = torch.tensor(relation_id).expand(self.node_len, 1).to(self.device)
+            head_id_vector = self.all_node_index_column_matrix.clone()
+            triplet_id_matric = torch.cat([head_id_vector, relation_id_vector, tail_id_vector], dim=1)
+            head_list = self._caculate_triplets_topk_node(triplet_id_matric)
+            return head_list
 
-        return head_list
 
 
     def predict_relation(self,head_id,tail_id):
@@ -257,5 +330,6 @@ class Kr_Predictior:
             relation_list.append(item)
 
         return relation_list
+
 
 
--- examples_demo/demo_eventkg2m_boxe.py
+++ examples_demo/demo_eventkg2m_boxe.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import RandomSampler
 from cogktr import *
+import time
 
 device=init_cogktr(device_id="3",seed=1)
 
@@ -18,11 +19,6 @@ train_dataset = processor.process(train_data)
 valid_dataset = processor.process(valid_data)
 test_dataset = processor.process(test_data)
 node_lut,relation_lut,time_lut=processor.process_lut()
-
-
-train_sampler = RandomSampler(train_dataset)
-valid_sampler = RandomSampler(valid_dataset)
-test_sampler = RandomSampler(test_dataset)
 
 model = BoxE(entity_dict_len=len(node_lut),
              relation_dict_len=len(relation_lut),
@@ -45,27 +41,40 @@ predictor=Kr_Predictior(model=model,
 ###########################################################################
 #                    以上为模型加载，以下为调用的接口
 ###########################################################################
-#'Copa Colombia'
-result_node=predictor.fuzzy_query_node_keyword()
-print(result_node)
+time_start = time.time()
 
-#"sport"
-result_relation=predictor.fuzzy_query_relation_keyword()
-print(result_relation)
+#模糊查询节点
+# result_node=predictor.fuzzy_query_node_keyword('Copa Colombia')
+# result_node=predictor.fuzzy_query_node_keyword()
+# print(result_node)
 
-similar_node_list=predictor.predict_similar_node(node_id=0)
+#模糊查询关系
+# result_relation=predictor.fuzzy_query_relation_keyword("sport")
+# result_relation=predictor.fuzzy_query_relation_keyword()
+# print(result_relation)
+
+#查询相似节点
+# similar_node_list=predictor.predict_similar_node(node_id=0)
 # print(similar_node_list)
-tail_list=predictor.predcit_tail(head_id=0,relation_id=0)
+
+#给出头节点和关系，查询尾节点
+# tail_list=predictor.predcit_tail(head_id=0,relation_id=0)
 # print(tail_list)
-head_list=predictor.predict_head(tail_id=0,relation_id=0)
+#给出头结点，查询关系和尾节点
+tail_list=predictor.predcit_tail(head_id=0)
+print(tail_list)
+
+#给出头节点和关系，查询尾节点
+# head_list=predictor.predict_head(tail_id=0,relation_id=0)
 # print(head_list)
-relation_list=predictor.predict_relation(head_id=0,tail_id=0)
+#给出尾节点，查询关系和头节点
+head_list=predictor.predict_head(tail_id=0)
+print(head_list)
+
+#给出头节点和尾节点，查询关系
+# relation_list=predictor.predict_relation(head_id=0,tail_id=0)
 # print(relation_list)
 
-
-
-
-
-
-
-
+time_end = time.time()
+time_c = time_end - time_start  # 运行所花时间
+print('time cost', time_c, 's')
