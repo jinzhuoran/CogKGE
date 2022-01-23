5547ca471c2148815500978e074b482dd7501689
Quester-one <2113809110@qq.com>
2022-01-23 16:21:46 +0800

    updata adapter_example

--- examples/adapter/example_adapter_graph.py
+++ examples/adapter/example_adapter_graph.py
@@ -6,9 +6,9 @@ FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0].parents[0].parents[0]  # CogKGE root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add CogKGE root directory to PATH
-from cogktr import *
+from cogkge import *
 
-device=init_cogktr(device_id="0",seed=1)
+device=init_cogkge(device_id="0",seed=1)
 
 loader =EVENTKG2MLoader(dataset_path="../../dataset",download=True)
 train_data, valid_data, test_data = loader.load_all_data()
@@ -51,7 +51,7 @@ negative_sampler = UnifNegativeSampler(triples=train_dataset,
                                        entity_dict_len=len(node_lut),
                                        relation_dict_len=len(relation_lut))
 
-trainer = Kr_Trainer(
+trainer = Trainer(
     train_dataset=train_dataset,
     valid_dataset=test_dataset,
     train_sampler=train_sampler,
