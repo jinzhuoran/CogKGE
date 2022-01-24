9b37d8c05899b68a51bff2087ef5a679a9c90d78
Quester-one <2113809110@qq.com>
2022-01-25 02:30:46 +0800

    init_cognet360k_examples

--- examples/cognet360k/test_example_cognet360k_simple.py
+++ examples/cognet360k/test_example_cognet360k_simple.py
@@ -11,7 +11,7 @@ if str(ROOT) not in sys.path:
 
 
 from cogkge import *
-device=init_cogkge(device_id="2",seed=1)
+device=init_cogkge(device_id="3",seed=1)
 
 loader =COGNET680KLoader(dataset_path="../../dataset",download=True)
 train_data, valid_data, test_data = loader.load_all_data()
