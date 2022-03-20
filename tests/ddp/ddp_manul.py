0c8d774b2d7fefc44e8c3f03fd6867ced1049403
HongbangYuan <1208314139@qq.com>
2022-03-20 17:17:16 +0800

    test spawn

--- tests/ddp/ddp_manul.py
+++ tests/ddp/ddp_manul.py
@@ -5,7 +5,7 @@ import torch
 import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
-import torch.multiprocessing as mps
+import torch.multiprocessing as mp
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
