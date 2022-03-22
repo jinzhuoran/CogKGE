97efb96f0752c6a8a2a67c4be4923576a9c32378
Quester-one <2113809110@qq.com>
2022-03-22 22:11:41 +0800

    update transh penalty

--- cogkge/models/transr.py
+++ cogkge/models/transr.py
@@ -135,14 +135,7 @@ class TransR(BaseModel):
         t_embedding = self.e_embedding(data[2])
         return h_embedding, r_embedding, t_embedding
 
-    def penalty(self,data):
-        batch_h, batch_r, batch_t = data[0], data[1], data[2]
-        h = self.h_embedding(batch_h)
-        r = self.r_embedding(batch_r)
-        t = self.t_embedding(batch_t)
-        r_matrix = self.matrix(batch_r)
-        penalty=(torch.mean(h ** 2) +torch.mean(t ** 2) +torch.mean(r ** 2) +torch.mean(r_matrix ** 2)) / 4
-        return penalty
+
 
     def loss(self, data):
         # 计算损失
