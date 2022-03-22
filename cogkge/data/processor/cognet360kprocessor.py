3f34d0e8785c12c7b8144434691d0bfa499b73fd
HongbangYuan <1208314139@qq.com>
2022-03-22 13:38:06 +0800

    update cognet processor

--- cogkge/data/processor/cognet360kprocessor.py
+++ cogkge/data/processor/cognet360kprocessor.py
@@ -2,5 +2,5 @@ from .baseprocessor import BaseProcessor
 
 
 class COGNET360KProcessor(BaseProcessor):
-    def __init__(self, node_lut, relation_lut):
-        super().__init__("COGNET360K", node_lut, relation_lut)
+    def __init__(self, node_lut, relation_lut,reprocess=True,train_pattern="score_based"):
+        super().__init__("COGNET360K", node_lut, relation_lut,reprocess=reprocess,train_pattern=train_pattern)
--- examples/cognet360k/test_example_cognet360k_rgcn.py
+++ examples/cognet360k/test_example_cognet360k_rgcn.py
@@ -2,47 +2,52 @@ import torch
 from torch.utils.data import RandomSampler
 from pathlib import Path
 import sys
+import numpy as np
 
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0].parents[0].parents[0]  # CogKGE root directory
+ROOT = FILE.parents[0].parents[0]  # CogKGE root directory
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add CogKGE root directory to PATH
 
-
 from cogkge import *
-device=init_cogkge(device_id="5",seed=1)
+
+device = init_cogkge(device_id="9", seed=1)
 
 loader =COGNET360KLoader(dataset_path="../../dataset",download=True)
 train_data, valid_data, test_data = loader.load_all_data()
 node_lut, relation_lut = loader.load_all_lut()
-# loader.describe()
-# train_data.describe()
-# node_lut.describe()
 
-processor = COGNET360KProcessor(node_lut, relation_lut)
+processor = COGNET360KProcessor(node_lut, relation_lut, reprocess=True,train_pattern="classification_based")
 train_dataset = processor.process(train_data)
 valid_dataset = processor.process(valid_data)
 test_dataset = processor.process(test_data)
 node_lut,relation_lut=processor.process_lut()
-# node_lut.print_table(front=3)
-# relation_lut.print_table(front=3)
 
 train_sampler = RandomSampler(train_dataset)
 valid_sampler = RandomSampler(valid_dataset)
 test_sampler = RandomSampler(test_dataset)
 
-model = TransE(entity_dict_len=len(node_lut),
-               relation_dict_len=len(relation_lut),
-               embedding_dim=50)
+print("Constructing adjacency matrix...")
+edge_index, edge_type = construct_adj(train_dataset, relation_dict_len=len(relation_lut))
+print("Adjacency matrix construction finished.")
 
-loss = MarginLoss(margin=1.0,C=0)
+model = RGCN(edge_index=edge_index,
+             edge_type=edge_type,
+             entity_dict_len=len(node_lut),
+             relation_dict_len=len(relation_lut),
+             embedding_dim=200,
+             )
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0)
+loss = torch.nn.BCELoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0)
 
 metric = Link_Prediction(link_prediction_raw=True,
-                         link_prediction_filt=False,
-                         batch_size=500000,
-                         reverse=False)
+                         link_prediction_filt=True,
+                         batch_size=50000,
+                         reverse=False,
+                         metric_pattern="classification_based")
+
 
 lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer, mode='min', patience=3, threshold_mode='abs', threshold=5,
@@ -55,22 +60,20 @@ negative_sampler = UnifNegativeSampler(triples=train_dataset,
 
 trainer = Trainer(
     train_dataset=train_dataset,
-    valid_dataset=valid_dataset,
+    valid_dataset=test_dataset,
     train_sampler=train_sampler,
-    valid_sampler=valid_sampler,
-    test_dataset=test_dataset,
-    test_sampler=test_sampler,
+    valid_sampler=test_sampler,
     model=model,
     loss=loss,
     optimizer=optimizer,
     negative_sampler=negative_sampler,
     device=device,
-    output_path="../../dataset",
+    output_path="../dataset",
     lookuptable_E=node_lut,
     lookuptable_R=relation_lut,
     metric=metric,
     # lr_scheduler=lr_scheduler,
-    trainer_batch_size=100000,
+    trainer_batch_size=2048*2,
     total_epoch=1000,
     apex=True,
     dataloaderX=True,
@@ -79,8 +82,7 @@ trainer = Trainer(
     use_tensorboard_epoch=100,
     use_matplotlib_epoch=100,
     use_savemodel_epoch=100,
-    use_metric_epoch=50
+    use_metric_epoch=50,
 )
 trainer.train()
 
- 
