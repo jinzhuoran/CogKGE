96ed61dae48091d7ea44e65d7f8fa587c12a5f9f
HongbangYuan <1208314139@qq.com>
2022-06-05 10:02:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from setuptools import setup
+import setuptools
 
+with open("requirements.txt",encoding="utf-8") as f:
+    reqs = f.read()
+pkgs=[]
 setup(
     name="cogkge",
-    version="0.1.0",
+    version="0.2.0",
     description="CogKGE: A Knowledge Graph Embedding Toolkit and Benckmark for Representing Multi-source and Heterogeneous Knowledge",
     url="https://github.com/jinzhuoran/CogKGE/",
     author="CogNLP Team",
+    packages=setuptools.find_packages(),
     author_email="zhuoran.jin@nlpr.ia.ac.cn",
     license='MIT',
-    install_requires=[
-        "torch==1.10.1+cu113",
-        "transformers==4.15.0",
-        "openTSNE",
-        "mongoengine",
-        "tensorboard==2.7.0",
-        "prettytable",
-        "pandas==1.3.5",
-        "prefetch-generator==1.0.1",
-        "matplotlib",
-    ],
+    install_requires=reqs.strip().split("\n"),
     python_requires=">=3.7,<3.9",
     zip_safe=False,
 )
